057b1b28547e5116745715a749baf92eee3d00b6
Altamash Khan <altamashidrisi9090@gmail.com>
2025-11-02 23:54:38 +0530

    any and unknown in ts

--- src/02-any-unknown.ts
+++ src/02-any-unknown.ts
@@ -5,4 +5,20 @@ someVariable = 12;
 
 someVariable.toFixed(3)
 
-console.log(someVariable)
+// console.log(someVariable)
+
+// console.log(someVariable.toFixed(3))
+
+// unknown
+let someVariable2: unknown;
+let boolVar: boolean;
+someVariable2 = 12;
+if (typeof someVariable2 === "number") {
+    console.log(someVariable2.toFixed(3))
+}
+
+someVariable2 = true;
+if(typeof someVariable2 === "boolean"){
+
+    boolVar = someVariable2
+}
